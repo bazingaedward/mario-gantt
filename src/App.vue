885486c78f02f2a0c0e87d654047903f7f24dec5
kaixiang.qiu <kaixiang.qiu@guwave.com>
2023-11-07 09:20:08 +0800

    feat: 添加dhx源代码

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,81 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { Gantt, DefaultTheme, MaterialTheme } from './tt'
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+const scales = [
+  { unit: 'month', step: 1, format: 'MMMM yyy' },
+  { unit: 'day', step: 1, format: 'd' }
+]
+
+const columns = [
+  { name: 'text', label: 'Task name', width: '100%' },
+  { name: 'start', label: 'Start time', align: 'center' },
+  { name: 'duration', label: 'Duration', width: '70px', align: 'center' },
+  { name: 'add-task', label: '', width: '50px', align: 'center' }
+]
+
+const tasks = [
+  {
+    id: 1,
+    open: true,
+    start_date: '2020-11-06',
+    duration: 8,
+    text: 'vue Gantt Widget222',
+    progress: 60,
+    type: 'project'
+  },
+  {
+    id: 2,
+    parent: 1,
+    start_date: '2020-11-06',
+    duration: 10,
+    text: 'Lib-Gantt',
+    progress: 80
+  }
+  // {
+  //   id: 3,
+  //   parent: 1,
+  //   text: 'release',
+  //   type: 'milestone'
+  // }
+]
+
+const links = [{ source: 2, target: 1, type: 0 }]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div style="width: 1256px; height: 1000px">
+    <MaterialTheme>
+      <Gantt :scales="scales" :columns="columns" :tasks="tasks" :links="links" />
+    </MaterialTheme>
+  </div>
 </template>
 
 <style scoped>
